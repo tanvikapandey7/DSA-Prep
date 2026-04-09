73b08e5d3666c08b5a460c43f39414b8bc6540b9
tanvika <tanvikapandey7@gmail.com>
2026-04-09 07:26:54 +0530

    three sum fixed i

---
+++ Two-pointer/ThreeSum.java
@@ -0,0 +1,13 @@
+public class threeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> result = threeSum(nums);
+        for(List<Integer> list : result) {
+            System.out.println(list);
+        }
+    }
+
+}
+public static List<List<Integer>> threeSum(int[] nums) {
+    
+}
